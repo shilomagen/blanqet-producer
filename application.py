cc6917ca28954b82554bb25d33db46bf5afe5627
shilomagen <smangam@gmail.com>
2020-06-06 21:11:26 +0300

    First commit

---
+++ application.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+def say_hello(username="World"):
+    return '<p>Hello %s!</p>\n' % username
+
+
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+@application.route('/')
+def index_route():
+    return header_text + say_hello() + instructions + footer_text
+
+
+@application.route('/<username>')
+def hello_route(username):
+    return header_text + say_hello(username) + home_link + footer_text
+
+
+if __name__ == '__main__':
+    application.run()
